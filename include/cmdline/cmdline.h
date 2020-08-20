64a6e212bfb2c62fecf195db1d6719653aceb2a3
Jakob <jakob@mohrbacher.eu>
2020-08-20 02:50:05 +0200

    initial_commit

---
+++ include/cmdline/cmdline.h
@@ -0,0 +1,220 @@
+#pragma once
+#include "stdafx.h"
+#include "arguments.h"
+
+namespace cmdline {
+
+struct Option {
+  char short_name;
+  std::string long_name;
+  std::string help;
+  std::string argument_name;
+
+  bool takes_argument;
+  size_t nargs;
+  std::function<bool(const char **)> set_value;
+};
+
+
+struct Argument {
+  std::string name;
+  std::string help;
+  bool required;
+
+  size_t nargs;
+  std::function<bool(const char **)> set_value;
+};
+
+
+class ArgumentParser {
+protected:
+  std::vector<Option> m_options;
+  std::vector<Argument> m_arguments;
+  bool m_show_help { false }; //< output for the help option
+  std::stringstream m_ss; //< used for converting arguments
+
+public:
+  /**
+   * Whether to show error messages.
+   */
+  bool error_messages = true;
+
+  /**
+   * Whether to allow abbreviations.
+   *
+   * If this is set to true, the argument `-fo' could match the option "foobar".
+   * If this is set to true, short option can not be grouped anymore.
+   */
+  bool abbreviations = false;
+
+  ArgumentParser();
+
+  /**
+   * @brief Adds a flag option.
+   */
+  bool add_option(bool &value, const char *help, char short_name, const char *long_name);
+  /**
+   * @brief Adds an option with one argument.
+   */
+  template<typename T>
+  bool add_option(T &value, const char *help, char short_name, const char *long_name, const char *value_name = "");
+  /**
+   * @brief Adds an option with one argument, multiple occurrences will all be stored.
+   */
+  template<typename T>
+  bool add_option(std::vector<T> &value, const char *help, char short_name, const char *long_name, const char *value_name = "");
+  /**
+   * @brief Adds an option with multiple arguments.
+   */
+  template<typename T, std::size_t N>
+  bool add_option(std::array<T, N> &value, const char *help, char short_name, const char *long_name, const char *value_name = "");
+
+
+  /**
+   * @brief Adds an argument with one value.
+   */
+  template<typename T>
+  void add_argument(T &value, const char *name, const char *help, bool required = true);
+  /**
+   * @brief Adds an argument with mutiple values
+   */
+  template<typename T, std::size_t N>
+  void add_argument(std::array<T, N> &value, const char *name, const char *help, bool required = true);
+
+
+  /**
+   * @brief Parses arguments.
+   */
+  int parse_args(int argc, const char **argv, bool exit_on_failure = true);
+
+  /**
+   * @brief Prints the usage text.
+   */
+  void usage(const char *);
+
+protected:
+  using ArgumentsIt = std::vector<Argument>::iterator;
+  static constexpr std::size_t npos = static_cast<std::size_t>(-1);
+
+  bool validate_option(char short_name, const char *long_name);
+
+  std::size_t option_index(char short_name);
+  std::size_t option_index(const std::string_view long_name);
+
+  bool parse_long_option(int, const char **, int &);
+  bool parse_short_option(int, const char **, int &);
+  bool parse_argument(int, const char **, int &, ArgumentsIt &);
+};
+
+///////////////////////////////////////////////////////////////////////////
+// Implementations of template functions
+
+template<typename T>
+bool ArgumentParser::add_option(T &value, const char *help, char short_name, const char *long_name, const char *value_name) {
+  if (!this->validate_option(short_name, long_name)) {
+    return false;
+  }
+  m_options.emplace_back(
+    short_name,
+    long_name,
+    help,
+    value_name,
+    true,
+    1,
+    [&](const char **arg) -> bool {
+      m_ss.clear();
+      m_ss.rdbuf()->str(*arg);
+      m_ss >> value;
+      return m_ss.rdbuf()->in_avail() == 0;
+    }
+  );
+  return true;
+}
+
+template<typename T>
+bool ArgumentParser::add_option(std::vector<T> &value, const char *help, char short_name, const char *long_name, const char *value_name) {
+  if (!this->validate_option(short_name, long_name)) {
+    return false;
+  }
+  m_options.emplace_back(
+    short_name,
+    long_name,
+    help,
+    value_name,
+    true,
+    1,
+    [&](const char **arg) -> bool {
+      T t;
+      m_ss.clear();
+      m_ss.rdbuf()->str(*arg);
+      m_ss >> t;
+      value.push_back(t);
+      return m_ss.rdbuf()->in_avail() == 0;
+    }
+  );
+  return true;
+}
+
+template<typename T, std::size_t N>
+bool ArgumentParser::add_option(std::array<T, N> &value, const char *help, char short_name, const char *long_name, const char *value_name) {
+  if (!this->validate_option(short_name, long_name)) {
+    return false;
+  }
+  m_options.emplace_back(
+    short_name,
+    long_name,
+    help,
+    value_name,
+    true,
+    N,
+    [&](const char **args) -> bool {
+      for (unsigned int i = 0; i < N; ++i) {
+        m_ss.clear();
+        m_ss.rdbuf()->str(args[i]);
+        m_ss >> value[i];
+        if (m_ss.rdbuf()->in_avail() != 0)
+          return false;
+      }
+      return true;
+    }
+  );
+  return true;
+}
+
+template<typename T>
+void ArgumentParser::add_argument(T &value, const char *name, const char *help, bool required) {
+  m_arguments.emplace_back(
+    name,
+    help,
+    required,
+    1,
+    [&](const char **arg) -> bool {
+      m_ss.clear();
+      m_ss.rdbuf()->str(*arg);
+      m_ss >> value;
+      return m_ss.rdbuf()->in_avail() == 0;
+    }
+  );
+}
+
+template<typename T, std::size_t N>
+void ArgumentParser::add_argument(std::array<T, N> &value, const char *name, const char *help, bool required) {
+  return m_arguments.emplace_back(
+    name,
+    help,
+    required,
+    N,
+    [&](const char **args) -> bool {
+      for (unsigned int i = 0; i < N; ++i) {
+        m_ss.clear();
+        m_ss.rdbuf()->str(args[i]);
+        m_ss >> value[i];
+        if (m_ss.rdbuf()->in_avail() != 0)
+          return false;
+      }
+      return true;
+    }
+  );
+}
+
+}
---
+++ source/cmdline.cpp
@@ -0,0 +1,356 @@
+#include "argumentparser.h"
+
+namespace cmdline {
+namespace detail {
+
+// Gets the display name for a option argument
+std::string get_argument_name(char short_name, const char *long_name) {
+  if (long_name != nullptr) {
+    std::string s(long_name);
+    std::for_each(s.begin(), s.end(), ::toupper);
+    return s;
+  }
+  else {
+    return std::string(1, ::toupper(short_name));
+  }
+}
+
+}
+
+
+ArgumentParser::ArgumentParser() {
+  this->add_option(m_show_help, "Display this message", 0, "help");
+}
+
+bool ArgumentParser::add_option(bool &value, const char *help, char short_name, const char *long_name) {
+  if (!this->validate_option(short_name, long_name)) {
+    return false;
+  }
+  m_options.emplace_back(
+    short_name,
+    long_name,
+    help,
+    "",
+    false,
+    0,
+    [&value](const char **) -> bool {
+      value = true;
+      return true;
+    }
+  );
+  return true;
+}
+
+
+bool ArgumentParser::validate_option(char short_name, const char *long_name) {
+  bool cs = short_name != 0;
+  bool cl = long_name[0] != '\0';
+  // Check if either of the names already exists
+  for (auto &o : m_options) {
+    if (cs and o.short_name == short_name) {
+      std::fprintf(stderr, "duplicate option -- %c\n", short_name);
+      return false;
+    }
+    if (cl and o.long_name == long_name) {
+      std::fprintf(stderr, "duplicate option `%s'\n", long_name);
+      return false;
+    }
+  }
+  return true;
+}
+
+int ArgumentParser::parse_args(int argc, const char **argv, bool exit_on_failure) {
+  auto arg_it = m_arguments.begin();
+  bool terminate_options = false;
+  int i;
+
+  auto print_usage_and_exit = [&]() {
+    this->usage(argv[0]);
+    if (exit_on_failure) {
+      std::exit(EXIT_FAILURE);
+    }
+  };
+
+  for (i = 1; i < argc; ++i) {
+    std::printf("ArgumentParser::parse_args: %d: \"%s\"\n", i, argv[i]);
+    if (!terminate_options and argv[i][0] == '-') {
+      /*if (!strcmp(argv[i], "--")) {
+        std::puts("ArgumentParser::parse_args: `--': stopping option parsing");
+        terminate_options = true;
+        continue;
+      }*/
+      if (argv[i][1] == '-'
+          or (abbreviations and (argv[i][2] or this->option_index(argv[i][1]) == npos))) {
+        if (!this->parse_long_option(argc, argv, i)) {
+          std::puts("  FAILED");
+          print_usage_and_exit();
+        }
+      }
+      else {
+        if (!this->parse_short_option(argc, argv, i)) {
+          std::puts("  FAILED");
+          print_usage_and_exit();
+        }
+      }
+    }
+    else {
+      if (!this->parse_argument(argc, argv, i, arg_it)) {
+          std::puts("  FAILED");
+        print_usage_and_exit();
+      }
+    }
+  }
+
+  return i;
+}
+
+std::size_t ArgumentParser::option_index(char short_name) {
+  for (std::size_t i = 0; i < m_options.size(); ++i) {
+    if (m_options[i].short_name == short_name) {
+      return i;
+    }
+  }
+  return npos;
+}
+
+std::size_t ArgumentParser::option_index(const std::string_view long_name) {
+  for (std::size_t i = 0; i< m_options.size(); ++i) {
+      if (m_options[i].long_name == long_name) {
+        return i;
+      }
+    }
+  return npos;
+}
+
+bool ArgumentParser::parse_long_option(int argc, const char **argv, int &optind) {
+  std::printf("ArgumentParser::parse_long_option(%d, %p(\"%s\"), %d)\n", argc, argv, argv[optind], optind);
+  const std::string_view tok(argv[optind]);
+  const std::size_t eq_pos = tok.find('=');
+  const int off = 1 + static_cast<int>(argv[optind][1] == '-');
+  const std::string_view name = tok.substr(off, eq_pos - off);
+  std::size_t index_found = npos;
+  bool ok = true;
+  std::size_t i;
+
+  if (!abbreviations) {
+    index_found = this->option_index(name);
+  }
+  else {
+    bool ambiguous = false;
+    for (i = 0; i < m_options.size(); ++i) {
+      if (!strncmp(name.data(), m_options[i].long_name.c_str(), name.length())) {
+        if (m_options[i].long_name.length() == name.length()) {
+          // exact match found
+          index_found = i;
+          ambiguous = false;
+          break;
+        }
+        else if (index_found == npos) {
+          // first non-exact match
+          index_found = i;
+        }
+        else {
+          ambiguous = true;
+        }
+      }
+    }
+    if (ambiguous) {
+      if (error_messages) {
+        std::fprintf(stderr, "%s: option `%s' is ambiguous\n", argv[0], argv[optind]);
+      }
+      return false;
+    }
+  }
+
+  if (index_found == npos) {
+    if (error_messages) {
+      std::cout << name << ' ' << name.length() << std::endl;
+      std::fprintf(stderr, "%s: unrecognized option `--%.*s'\n", argv[0], (int)name.length(), name.data());
+    }
+    return false;
+  }
+
+  Option &opt = m_options[index_found];
+
+  auto print_arg_error = [&]() {
+    if (opt.nargs == 1) {
+      std::fprintf(stderr, "%s: option `%s' requires an argument\n",
+          argv[0], argv[optind]);
+    }
+    else {
+      std::fprintf(stderr, "%s: option `%s' requires %zu arguments\n",
+          argv[0], argv[optind], opt.nargs);
+    }
+  };
+
+  if (opt.takes_argument) {
+    if (eq_pos == std::string::npos) {
+      // Check if there are enough argv elements left
+      if ((optind + opt.nargs) >= static_cast<std::size_t>(argc)) {
+        if (error_messages) {
+          print_arg_error();
+        }
+        return false;
+      }
+      // Check if there are enough arguments
+      for (std::size_t n = 0; n < opt.nargs; ++n) {
+        if (argv[optind + n + 1][0] == '-') {
+          if (error_messages) {
+            print_arg_error();
+          }
+          return false;
+        }
+      }
+      // All good
+      ok = opt.set_value(&argv[optind + 1]);
+      optind += opt.nargs;
+    }
+    else {
+      if (opt.nargs > 1) {
+        if (error_messages) {
+          std::fprintf(stderr, "%s: option `--%s' requires %zu arguments\n",
+            argv[0], argv[optind], opt.nargs);
+        }
+        return false;
+      }
+      const char *arg = argv[optind] + eq_pos + 1;
+      ok = opt.set_value(&arg);
+    }
+  }
+  else {
+    opt.set_value(nullptr);
+  }
+
+  return ok;
+}
+
+bool ArgumentParser::parse_short_option(int argc, const char **argv, int &optind) {
+  std::printf("ArgumentParser::parse_short_option(%d, %p(\"%s\"), %d)\n", argc, argv, argv[optind], optind);
+  bool ok = true;
+  std::size_t index = this->option_index(argv[optind][1]);
+
+  if (index == npos) {
+    if (error_messages) {
+      std::fprintf(stderr, "%s: invalid option -- %c\n", argv[0], argv[optind][1]);
+    }
+    return false;
+  }
+
+  Option &opt = m_options[index];
+
+  auto print_arg_error = [&]() {
+    if (opt.nargs == 1) {
+      std::fprintf(stderr, "%s: option requires an argument -- %c\n",
+          argv[0], argv[optind][1]);
+    }
+    else {
+      std::fprintf(stderr, "%s: option requires %zu arguments -- %c\n",
+          argv[0], opt.nargs, argv[optind][1]);
+    }
+  };
+
+  if (opt.takes_argument) {
+    if (argv[optind][2]) {
+      // There's something else in the argv element, assume it's the argument
+      if (opt.nargs > 1) {
+        if (error_messages) {
+          std::fprintf(stderr, "%s: option requires %zu arguments -- %c\n",
+            argv[0], opt.nargs, argv[optind][1]);
+        }
+        return false;
+      }
+      const char *arg = argv[optind] + 2;
+      ok = opt.set_value(&arg);
+    }
+    else {
+      // Check if there are enough argv elements left
+      if ((optind + opt.nargs) >= static_cast<std::size_t>(argc)) {
+        if (error_messages) {
+          print_arg_error();
+        }
+        return false;
+      }
+      // Check if there are enough arguments
+      for (std::size_t n = 0; n < opt.nargs; ++n) {
+        if (argv[optind + n + 1][0] == '-') {
+          if (error_messages) {
+            print_arg_error();
+          }
+          return false;
+        }
+      }
+      // All good
+      ok = opt.set_value(&argv[optind + 1]);
+      optind += opt.nargs;
+    }
+  }
+  else {
+    opt.set_value(nullptr);
+    for (std::size_t i = 2; i < std::strlen(argv[optind]); ++i) {
+      index = this->option_index(argv[optind][i]);
+      if (index == npos) {
+        if (error_messages) {
+          std::fprintf(stderr, "%s: invalid option -- %c\n", argv[0], argv[optind][1]);
+        }
+        return false;
+      }
+      if (m_options[index].takes_argument) {
+        // Options taking an argument can not be grouped
+        if (error_messages) {
+          std::fprintf(stderr, "%s: option requires an argument -- %c\n", argv[0], argv[optind][i]);
+        }
+        return false;
+      }
+      m_options[index].set_value(nullptr);
+    }
+  }
+
+  return ok;
+}
+
+bool ArgumentParser::parse_argument(int argc, const char **argv, int &optind, ArgumentsIt &arg_it) {
+  std::printf("ArgumentParser::parse_argument(%d, %p(\"%s\"), %d, <it>)\n", argc, argv, argv[optind], optind);
+  if (arg_it == m_arguments.end()) {
+    return true;
+  }
+  Argument &arg = *arg_it;
+
+  auto print_arg_error = [&]() {
+    if (arg.nargs == 1) {
+      std::fprintf(stderr, "%s: argument `%s' requires an argument\n",
+          argv[0], arg.name.c_str());
+    }
+    else {
+      std::fprintf(stderr, "%s: argument `%s' requires %zu arguments\n",
+          argv[0], arg.name.c_str(), arg.nargs);
+    }
+  };
+
+  // Check if there are enough argv elements left
+  if ((optind + arg.nargs) >= static_cast<std::size_t>(argc)) {
+    if (error_messages) {
+      print_arg_error();
+    }
+    return false;
+  }
+  // Check if there are enough arguments
+  for (std::size_t n = 0; n < arg.nargs; ++n) {
+    if (argv[optind + n + 1][0] == '-') {
+      if (error_messages) {
+        print_arg_error();
+      }
+      return false;
+    }
+  }
+  // All good
+  arg.set_value(&argv[optind + 1]);
+  optind += arg.nargs;
+  ++arg_it;
+  return true;
+}
+
+void ArgumentParser::usage(const char *program_name) {
+  std::fprintf(stderr, "%s: usage\n", program_name);
+}
+
+}

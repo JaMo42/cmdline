54d498042a0cc0058694298bbdb8ebb03a86fb39
Jakob <jakob@mohrbacher.eu>
2020-08-20 15:12:53 +0200

    some cleanup from restructuring project

--- source/cmdline.cpp
+++ source/cmdline.cpp
@@ -1,4 +1,25 @@
-#include "argumentparser.h"
+/*
+Copyright (c) 2020 Jakob Mohrbacher
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include "cmdline.h"
 
 namespace cmdline {
 namespace detail {
@@ -16,7 +37,6 @@ std::string get_argument_name(char short_name, const char *long_name) {
 }
 
 }
-
 
 ArgumentParser::ArgumentParser() {
   this->add_option(m_show_help, "Display this message", 0, "help");
@@ -164,7 +184,6 @@ bool ArgumentParser::parse_long_option(int argc, const char **argv, int &optind)
 
   if (index_found == npos) {
     if (error_messages) {
-      std::cout << name << ' ' << name.length() << std::endl;
       std::fprintf(stderr, "%s: unrecognized option `--%.*s'\n", argv[0], (int)name.length(), name.data());
     }
     return false;
